6988125f7b714d362f328b9404c09270598c1144
unknown <jyek121242@gmail.com>
2025-06-23 14:03:38 +0900

    new

--- src/components/page/profile/components/modalBox.component.tsx
+++ src/components/page/profile/components/modalBox.component.tsx
@@ -1,7 +1,6 @@
 import { useRef } from "react";
 import styles from "./modalBox.module.scss";
 import { useAnimateOnClass } from "./hooks/useAnimateOnClass";
-import Image from 'next/image';
 interface ModalBoxProps {
     selectedTitle: string | null;
 }
@@ -22,69 +21,15 @@ export default function ModalBox({ selectedTitle }: ModalBoxProps) {
                 <ul className={styles.modal_box01}>
                     <li className={styles.first_figma}>
                         <ul>
-                            <li>
-                                <div className={styles.img01_box}>
-                                    <Image
-                                        src="/images/sass.png"
-                                        alt="sass"
-                                        fill
-                                    />
-                                </div>
-                            </li>
-                            <li>
-                                <div className={styles.img02_box}>
-                                    <Image
-                                        src="/images/typescript.png"
-                                        alt="typescript"
-                                        fill
-                                    />
-                                </div>
-                            </li>
-                            <li>
-                                <div className={styles.img03_box}>
-                                    <Image
-                                        src="/images/nodejs.png"
-                                        alt="nodejs"
-                                        fill
-                                    />
-                                </div>
-                            </li>
-                            <li>
-                                <div className={styles.img04_box}>
-                                    <Image
-                                        src="/images/nextjs.png"
-                                        alt="nextjs"
-                                        fill
-                                    />
-                                </div>
-                            </li>
-                            <li>
-                                <div className={styles.img05_box}>
-                                    <Image
-                                        src="/images/vercel.png"
-                                        alt="vercel"
-                                        fill
-                                    />
-                                </div>
-                            </li>
+                            <li className={styles.img01}></li>
+                            <li className={styles.img02}></li>
+                            <li className={styles.img03}></li>
+                            <li className={styles.img04}></li>
+                            <li className={styles.img05}></li>
                         </ul>
                     </li>
-                    <li className={styles.second_figma}>
-                        <div className={styles.second_figma_box}>
-                            <Image
-                                src="/images/user_flow.png"
-                                alt="user_flow"
-                                fill
-                            />
-                        </div>
-                        <div className={styles.last_figma_box}>
-                            <Image
-                                src="/images/main_figam.jpg"
-                                alt="main_figam"
-                                fill
-                            />
-                        </div>
-                    </li>
+                    <li className={styles.second_figma}></li>
+                    <li className={styles.last_figma}></li>
                 </ul>
             )}
             {selectedTitle === "PHOTOSHOP" && (
